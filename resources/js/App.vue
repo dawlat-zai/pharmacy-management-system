189c7ee82fcd0c7d57f30515cf71b5c92ee20aa7
dawlat <dawlatzai2006@gmail.com>
2023-11-01 13:05:51 +0500

    Added Vue.js

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>
+        Welcome to pharmacy management system
+    </h1>
+</template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,8 +9,11 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        @vite('resources/css/app.css')
     </head>
     <body>
-        <h1>Welcome to Pharmacy Management System</h1>
+        <div id="app"></div>
+        @vite('resources/js/app.js')
     </body>
 </html>
